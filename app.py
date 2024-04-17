7fce8761bf505ed963d7fba5f43a77a416b8d998
@iamserda <iamserda@gmail.com>
2024-04-17 00:09:52 -0400

    app: a book manager for the books I have home. -@iamserda

--- app.py
+++ app.py
@@ -1,95 +1,37 @@
-import copy
 from flask import Flask, request
+import json
 
+app = Flask("__name__")
 
-app = Flask(__name__)
-
-stores = [
-    {   "store_id": 1,
-        "name": "bronx",
-        "items": [{"name": "Chair", "price": 125.99}, {"name": "Table", "price": 225.99}]
-    },
-    {
-        "store_id": 2,
-        "name": "brooklyn",
-        "items": [{"name": "Milk", "price": 55.99}, {"name": "Eggs", "price": 22.99}]
-    },
-    {
-        "store_id": 3,
-        "name": "manhattan",
-        "items": [{"name": "Milk", "price": 55.99}, {"name": "Eggs", "price": 22.99}]
+inwood_library = {
+    "books": [{"id": 1, "title": "1984", "author": "George Orwell", "publisher": "Penguin"}],
+    "movies": [],
+    "events": [],
     }
-]
 
-@app.get("/") # http://example.com/
-def get_homepage():
-    """returns all items within store array"""
-    return '<html><head><meta name="title" content="Build REST APIs with Flask and Python (The Complete Course)"></head><body><h1>The REST store app</body</html>', 200
+@app.route("/api/books/")
+def get_books():
+    return { "books": inwood_library["books"]}
 
-@app.get("/store") #http://example.com/store
-def get_all_stores():
-    """returns container stores"""
-    return {"stores": stores}, 200
+@app.route("/api/book/<int:id>")
+def get_book_by_id(id):
+    for book in inwood_library.get("books"):
+        if book["id"] == id:
+            return {"book": book}
+        else:
+            return {"message": "404: Not Found!"}, 404
 
-@app.get("/store/<string:name>") #http://example.com/store/brooklyn
-def get_store_by_name(name):
-    """returns a store based on its name."""
-    result = {"store": None}
-    for item in stores:
-        if item["name"] == name:
-            result = item
-            return result, 200
-    return result,404
+@app.post("/api/book")
+def add_new_book():
+    book_info = request.get_json()
+    new_book = {"id": len(inwood_library["books"]) + 1}
+    for k,v in book_info.items():
+        new_book[k] = v    
+    inwood_library["books"].append(new_book)
+    return inwood_library["books"][-1]
+    
 
-@app.get("/store/<int:store_id>")
-def get_store_by_id(store_id):
-    """returns a store based on its id(index + 1)item."""
-    result = {"store": None}
-    for store in stores:
-        if store["store_id"] == store_id:
-            result["store"] = store
-            return result, 200
-    return result,404
 
-@app.post("/store")
-def add_new_store():
-    """returns the most recently added item."""
-    req_data = request.get_json()
-    new_store = {
-        "store_id": len(stores) + 1,
-        "name": req_data["name"],
-        "items": req_data["items"]
-    }
-    print(new_store)
-    stores.append(new_store)
-    return stores[-1], 201
-
-@app.post("/store/<string:store_name>/item")
-def add_new_item(store_name):
-    requested_data = request.get_json()
-    item_name, item_price = requested_data["name"], requested_data["price"]
-    for store in stores:
-        if store["name"] == store_name:
-            store["items"].append({"name": item_name, "price": item_price})
-            return store, 201
-    return {"message": "Store was not found or Invalid store."}, 404
-
-@app.get("/store/<string:store_name>/item")
-def get_store_items(store_name):
-    for store in stores:
-        if store["name"] == store_name:
-            return {"items": store["items"]}, 200
-    return {"message": "Store not found!"}, 404
-
-@app.post("/store/<string:store_name>/<string:item_name>/<float:new_price>")
-def update_item_price(store_name, item_name, new_price):
-    store = [store for store in stores if store["name"] == store_name]
-    if not store:
-        return {"message": "Store not found!"}, 404
-    old_store = copy.deepcopy(store[0])
-    for item in store[0]["items"]:
-        if item["name"] == item_name:
-            item["price"] = float(new_price)
-            return {"before": old_store, "after": store},201
-    return {"message": "Item not found!"}, 404
-
+@app.get("/api/movies/")
+def get_movies():
+    return { "movies": inwood_library["movies"]}
